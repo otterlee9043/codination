2319c1d29b48c1a7ac64ebd5def4453cabc80c73
otterlee9043 <otterlee99@gmail.com>
2022-03-10 01:48:41 +0900

    Add drag event handler

--- script.js
+++ script.js
@@ -105,3 +105,25 @@ window.addEventListener("load", function () {
     });
   });
 });
+
+function selectText() {
+  var selectionText = "";
+  if (document.getSelection) {
+    selectionText = document.getSelection();
+  } else if (document.selection) {
+    selectionText = document.selection.createRange().text;
+  }
+  return selectionText;
+}
+
+window.addEventListener(
+  "mouseup",
+  (event) => {
+    console.log(event.toElement.parentElement.innerText);
+    const selectedString = selectText().toString();
+    const selectedLines = selectedString.split("\n");
+    console.log(selectedLines);
+    // console.log();
+  },
+  false
+);
